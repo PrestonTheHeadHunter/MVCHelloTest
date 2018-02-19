b2a92c81451b8d6e538bf05ee87b3d394956722b
Preston Hale <preston.hale@avanade.com>
2018-02-19 09:55:47 -0600

    changes

--- MVCHelloTest/Controllers/HomeController.cs
+++ MVCHelloTest/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MVCHelloTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "It's All About me!";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact me";
 
             return View();
         }
